0f054e83d41215ea410ea410e5b9a298db0e8243
iamkhushia <161837209+iamkhushia@users.noreply.github.com>
2024-03-05 17:27:02 +0530

    Add files via upload

---
+++ datatype.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+main()
+{
+	int a=11;
+	int b=24;
+	
+	printf("\tA=%d\n",a);
+	printf("\tB=%d\n\n",b);
+	printf("\tA+B=%d\n\n",a+b);
+	
+	printf("\tA-B=%d\n\n",a-b);
+	
+	printf("\tA*B=%d\n\n",a*b);
+	
+	printf("\tA/B=%d\n\n",a/b);
+	
+	printf("\tA%%B=%d\n",a%b);
+}
